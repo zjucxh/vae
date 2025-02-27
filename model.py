417857dbd6076a18ac364fd0f9c71052fe766cc4
zjucxh <chenxianghui@zju.edu.cn>
2025-02-27 12:37:30 +0800

    Feat!: Add GRU module for vertex displacement prediction

--- model.py
+++ model.py
@@ -69,6 +69,24 @@ class VAE(nn.Module):
         
         return sdist , mu, logvar
 
+# Define Gated Recurrent Unit
+class GRU(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers=4, output_dim=289*3):
+        super(GRU, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim 
+        self.num_layers = num_layers
+        self.gru = nn.GRU(input_dim, hidden_dim, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+    
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).to('cuda')
+        #print(f' h0 shape : {h0.shape}')
+        out, _ = self.gru(x, h0)
+        out = self.fc(out[:,-1,:])
+        return out
+
 # Define the loss function for the VAE
 def vae_loss(recon_x, x, mu, logvar):
     # Reconstruction loss
@@ -92,21 +110,32 @@ def loss_l1(pred_distance, gt_distance, clamp=0.1):
 if __name__=='__main__':
     # load data
     dataset = Cloth_in_Wind()
-    dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
     input_dim = 289 * 3
-    hidden_dim = 256
-    latent_dim = 1024 
-    vae_model = VAE(input_dim, hidden_dim, latent_dim).to(device='cuda')
-    # Initialize the VAE model
-    for i, data in enumerate(dataloader):
-        x, sdist, noise, nsdist = data
-        x = x.view(x.size(0), -1).to('cuda')
-        y, mu, var = vae_model(x)
-        print('y shape : {}'.format(y.shape))
+    batch_size = 8
+    seq_length = 4
+    dataloader = DataLoader(dataset, batch_size=seq_length+1, shuffle=False)
     
 
-    sdist , mu, logvar = vae_model(x)
-    print(' sdist shape : {}'.format(sdist.shape))
-    print(' mu shape : {}'.format(mu.shape))
-    print(' logvar shape : {}'.format(logvar.shape))
+    # Load GRU model
+    gru = GRU(input_dim=input_dim, hidden_dim=256, num_layers=4, output_dim=input_dim).to('cuda')
+    print(f' gru model : {gru}')
+    optimizer = torch.optim.Adam(gru.parameters(), lr=1.0e-5)
+    critrion = nn.MSELoss()
+    data = torch.empty(batch_size, seq_length, input_dim).to('cuda')
+    for epoch in range(1000):
+
+        for batch in range(batch_size):
+            for i, vertices in enumerate(dataloader):
+                vertices = vertices.reshape(vertices.shape[0], -1)
+                data[batch] = vertices[:seq_length].to('cuda')
+                gt = vertices[seq_length].to('cuda')
+                outputs = gru(data)
+                # l2 loss
+                loss = critrion(outputs, gt)
+                print(f' loss : {loss.item()}')
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+            
     print('Done')
