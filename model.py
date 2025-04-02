be93eb5880064e0a9b6eaaf4a8921fec06309e89
zjucxh <chenxianghui@zju.edu.cn>
2025-04-02 16:57:19 +0800

    Wip: Neural level set method

--- model.py
+++ model.py
@@ -20,53 +20,6 @@ class ResNetBlock(nn.Module):
     def forward(self, x):
         return x + self.block(x)
 
-# Define the VAE model
-class VAE(nn.Module):
-    def __init__(self, input_dim, hidden_dim, latent_dim):
-        super(VAE, self).__init__()
-        
-        self.input_dim = input_dim # num of sampled points * 3
-        self.hidden_dim = hidden_dim # num of hidden units
-        self.latent_dim = latent_dim # num of latent variables
-        self.res_depth = 50# num of resnet blocks
-        
-        # Encoder layers
-        self.encoder = nn.Sequential(
-            nn.Linear(input_dim, hidden_dim),
-            nn.ReLU(),
-            *[ResNetBlock(hidden_dim) for _ in range(self.res_depth)],
-            nn.Linear(hidden_dim, latent_dim * 2)  # Two sets of outputs for mean and variance
-        )
-        # Modify encoder to resnet18
-        #self.encoder = resnet18(pretrained=False)
-        
-        # Decoder layers
-        self.decoder = nn.Sequential(
-            nn.Linear(latent_dim, hidden_dim),
-            nn.ReLU(),
-            *[ResNetBlock(hidden_dim) for _ in range(self.res_depth)],
-            nn.Linear(hidden_dim, hidden_dim),
-            nn.ReLU(),
-            nn.Linear(hidden_dim, input_dim)
-        )
-    
-    def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5*logvar)
-        eps = torch.randn_like(std)
-        return mu + eps*std
-
-    def forward(self, x):
-        # Encode
-        enc = self.encoder(x)
-        mu, logvar = enc[:,:self.latent_dim], enc[:,self.latent_dim:]
-        
-        # Reparameterization trick
-        z = self.reparameterize(mu, logvar)
-        # Decode
-        sdist = self.decoder(z)
-        
-        return sdist , mu, logvar
-
 # Define Gated Recurrent Unit
 class GRU(nn.Module):
     def __init__(self, input_dim, hidden_dim, num_layers=4, output_dim=289*3):
@@ -92,10 +45,10 @@ class NLS(nn.Module):
         super(NLS, self).__init__()
         r"""
         Args:
-            input_dim: int, the input dimension of NLS
+            input_dim: int, the input dimension of NLS (B, S, D) where D  is concated with t, pose and vertex
             hidden_dim: int, the number of hidden units in the GRU
             num_layers: int, the number of layers in the GRU
-            output_dim: int, the output dimension of NLS
+            output_dim: int, the output dimension of NLS signed distance of the vertex
         """
         # The input dimension of NLS is (B, S, D),
         # where B is the batch size, S is the sequence length, and D is the input dimension.
@@ -105,36 +58,58 @@ class NLS(nn.Module):
         self.hidden_dim = hidden_dim
         self.output_dim = output_dim
         self.num_layers = num_layers
+        self.force_field = ForceField(input_dim-1, num_layers=6, output_dim=3).to(device='cuda') # exclude time dimension
         self.gru = nn.GRU(input_dim, hidden_dim, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_dim, output_dim)
 
-    def forward(self, vertices, poses):
-        # The input of NLS is the concatenation of vertices and poses.
-        # vertices is the vertex sequence with shape (B, S, V*3),
+    def forward(self, t, poses, vertices):
+        # The input of NLS is the concatenation of time, vertices and poses.
+        # vertices is the vertex sequence with shape (B, S, V*3), poses shpae (B, S, 181), temporal shape (B, S, 1)
         # where B is the batch size, S is the sequence length, and V is the number of vertices.
         # The poses is the pose sequence with shape (B, S, pose_dim), pose_dim is 181
-        x = torch.cat([vertices, poses], dim=-1)
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).to('cuda')
-        out, _ = self.gru(x, h0)
+        x0 = torch.cat([t, poses,vertices], dim=-1).to(device='cuda')  # input of NLS
+        x1 = torch.cat([poses, vertices], dim=-1).to(device='cuda') # input of force field
+        h0 = torch.zeros(self.num_layers, x0.size(0), self.hidden_dim).to('cuda')
+        out, _ = self.gru(x0, h0) 
+        sdist = self.fc(out)# output of signed distance
+        # feed the force field to the model
+        ff = self.force_field(x1)
+        return sdist, ff
+    
+# TODO define Forcefield Model 
+class ForceField(nn.Module):
+    def __init__(self, input_dim, num_layers=6, output_dim=3):
+        r"""
+        Attention layer based on the paper "Attention is all you need"
+        Args:
+            input_dim: int, the input dimension of the model containing SMPL parameters and vertex sequence
+            num_layers: int, the number of layers in the model
+            output_dim: int, the output dimension of the model containing the force field consistent with the vertex sequence
+        """
+        super(ForceField, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.num_layers = num_layers
+        #self.attention = nn.MultiheadAttention(input_dim, num_heads=8, batch_first=True)
+        self.transformer = nn.TransformerEncoderLayer(input_dim, nhead=8, dim_feedforward=1024, batch_first=True) # transformer layer
+        self.encoder = nn.TransformerEncoder(self.transformer, num_layers=num_layers) # transformer encoder based on paper "Attention is all you need"
+        self.fc = nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        # The input of the model is the concatenation of SMPL parameters and vertex sequence.
+        # x is the input with shape (B, S, D), where B is the batch size, S is the sequence length, and D is the input dimension.
+        out = self.encoder(x)
         out = self.fc(out)
         return out
+    
 
-# Define the loss function for the VAE
-def vae_loss(recon_x, x, mu, logvar):
-    # Reconstruction loss
-    #sdist_loss = loss_l1(recon_x, x)
-    sdist_loss = F.mse_loss(recon_x, x, reduction='sum')
-
-    # KL divergence loss
-    kl_divergence = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-
-    return sdist_loss + kl_divergence
 
 # loss_l1
 def loss_l1(pred_distance, gt_distance, clamp=0.1):
     l1_loss = nn.L1Loss()
-    pred_distance = torch.clamp(pred_distance, -clamp, clamp)
-    gt_distance = torch.clamp(gt_distance, -clamp, clamp)
+    #print(f' pred distance : {pred_distance}')
+    #pred_distance = torch.clamp(pred_distance, -clamp, clamp)
+    #gt_distance = torch.clamp(gt_distance, -clamp, clamp)
     loss = l1_loss(pred_distance, gt_distance)
     return loss
 
@@ -163,6 +138,29 @@ def loss_laplacian(laplacian_matrix:torch.Tensor, pred_vertices:torch.Tensor, gt
     vertex_loss = loss_l2(pred_vertices, gt_vertices)
     return ratio * laplacian_loss + (1-ratio) * vertex_loss
 
+# Define NLS loss
+def loss_nls(model, gt_sdf,t, poses, sampled_vertex):
+    t.requires_grad_(True)
+    sampled_vertex.requires_grad_(True)
+    poses.requires_grad_(True)
+    
+    pred_sdf, ff = model(t, poses, sampled_vertex)
+
+    # compute the signed distance loss
+    sdf_loss = loss_l1(pred_sdf, gt_sdf)
+    # compute level set constraint loss \partial_t \phi + \nabla \phi \cdot f = 0
+    #sdf_t, _, sdf_x = torch.autograd.grad(pred_sdf, [t, poses, sampled_vertex], grad_outputs=torch.ones_like(pred_sdf), create_graph=True)
+    ##print(f' sdf_t shape : {sdf_t.shape}')
+    ##print(f' sdf_x shape : {sdf_x.shape}')
+    ##print(f' ff shape : {ff.shape}')
+    ## level set constraints loss
+    #nibla = torch.sum(sdf_x * ff, dim=-1).unsqueeze(dim=-1)
+    ##print(f' nibla : {nibla.shape}')
+    #level_set_loss = torch.mean(sdf_t + nibla)
+    #overall_loss = sdf_loss + level_set_loss
+    return sdf_loss
+
+
 
 if __name__=='__main__':
     # load data
@@ -171,51 +169,49 @@ if __name__=='__main__':
     template_faces = dataset.template_faces
     laplacian_matrix = dataset.laplacian_matrix.to(device='cuda')
 
-    input_dim = 181
-    output_dim = 2590*3
+    input_dim = 181 + 3 + 1
+    output_dim = 3
     batch_size = 8
     seq_length = 130
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
     num_epoches = 15000
     
+    # sample random vertex from the vertex sequence,
+    #torch.manual_seed(0)
 
-    # Load GRU model
-    gru = GRU(input_dim=input_dim, hidden_dim=512, num_layers=8, output_dim=output_dim).to('cuda')
-    print(f' gru model : {gru}')
-    optimizer = torch.optim.Adam(gru.parameters(), lr=1.0e-5)
-    critrion = loss_laplacian
+    # Load Force field model
+    #force_field = ForceField(input_dim-1, num_layers=6, output_dim=3).to(device='cuda') # exclude time dimension
+    # Load NLS model
+    nls = NLS(input_dim, hidden_dim=256, num_layers=4, output_dim=1).to(device='cuda')
+
+    optimizer = torch.optim.Adam(nls.parameters(), lr=1.0e-5)
+    #critrion = loss_laplacian
     # Train the model
     for epoch in range(num_epoches):
         running_loss = 0.0
         for i, data in enumerate(dataloader):
-            # reshape data to batch_size, seq_length, input_dim
-            gender, poses, vertex_seq = data
-            #print(f' ----vertex_seq shape : {vertex_seq.shape}')
-            vertex_seq = vertex_seq.to(device='cuda')
-            #vertex_seq = vertex_seq.view(-1, seq_length, 7770).to(device='cuda')
-            poses = poses.to(device='cuda')
-            # reshape beta from batch_size, 16 to batch_size, sequence_length, 16
+            # zero grad optimizer
             optimizer.zero_grad()
-            output = gru(poses)
-            # reshape output as vertex_seq
-            output = output.view_as(vertex_seq)
-            #print(f' out shape : {output.shape}')
-            #print(f' ground truth shape : {vertex_seq.shape}')
-            #loss_lap = loss_laplacian(laplacian_matrix, output, vertex_seq)
-            #print(f' laplacian loss : {loss_lap}')
-            loss = critrion(laplacian_matrix, output, vertex_seq)
-            running_loss += loss.item()
-            #print(f' loss : {loss }')
-            #print(f' i : {i} loss : {loss.item()}')
-            if i % 10 == 9:
-                print(f'[{epoch + 1}, {i + 1}] loss: {running_loss / 10}')
-                running_loss = 0.0
+            # reshape data to batch_size, seq_length, input_dim
+            gender, poses, vertex_seq = data # vertex shape (B, S, num_v, 3)
             
+            rand_idx = torch.randint(0, 2590, size=(1,))
+            sampled_vertex = vertex_seq[:, :,0].to(device='cuda')
+            #print(f' sampled index : {rand_idx}')
+
+            # generate time data shaped with (B, S, 1)
+            t = torch.ones(poses.shape[0], seq_length, 1).to(device='cuda')
+            poses = poses.to(device='cuda')
+            gt_sdf = torch.zeros(poses.shape[0], seq_length, 1).to(device='cuda')
+            
+            # compute loss
+            loss = loss_nls(nls, gt_sdf, t, poses, sampled_vertex)
+
+            # backward
             loss.backward()
             optimizer.step()
+            print(f' epoch : {epoch}, loss : {loss.item()}')
 
-        # save model for every 100 epoches
-        #if epoch % 100 == 99:
-        #   torch.save(gru.state_dict(), '/home/cxh/tmp/checkpoint/gru_{0:0>3}.pth'.format((epoch-99)//100))
+
         
     print('Done')
